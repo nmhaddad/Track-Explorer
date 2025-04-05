267b5483e81917b9df1f3a7970a806790e7b5184
Nathaniel Haddad <nhaddad2112@gmail.com>
2025-04-05 12:03:17 -0400

    Add new tool - generate_image_caption

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,12 @@
 ## Known Issues:
 - None
 
+## v0.2.1 - Nate Haddad, 4/5/2025
+- Add new `tool` for getting image caption using OpenAI
+
+## v0.2.0 - Nate Haddad, 4/5/2025
+- Add updated Gradio app
+
 ## v0.1.1 - Nate Haddad, 3/31/2025
 - `LangChainAnalyst`:
     - Add new system prompt with few-shot examples (many)
--- track_explorer/analysts/smolagents_analyst/tools.py
+++ track_explorer/analysts/smolagents_analyst/tools.py
@@ -2,6 +2,7 @@
 
 import os
 
+from openai import OpenAI
 from smolagents import tool
 from sqlalchemy import create_engine, text
 
@@ -9,7 +10,7 @@ from sqlalchemy import create_engine, text
 @tool
 def sql_engine(query: str) -> str:
     """
-    "Allows you to perform SQL queries on the table. Beware that this tool's output is a string representation of the execution output.
+    Allows you to perform SQL queries on the table. Beware that this tool's output is a string representation of the execution output.
     It can use the following tables:
 
     Args:
@@ -22,3 +23,41 @@ def sql_engine(query: str) -> str:
         for row in rows:
             output += "\n" + str(row)
     return output
+
+
+@tool
+def generate_image_caption(image_base64: str) -> str | None:
+    """
+    Allows you to generate a caption for an image. This is useful for describing the contents of a frame in a video or
+    an image. Use this tool sparingly, as it can be slow and expensive.
+
+    Args:
+        image_base64 (str): The base64 encoded string representation of the image.
+    """
+    try:
+        client = OpenAI()
+        response = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "text",
+                            "text": "Describe the image.",
+                        },
+                        {
+                            "type": "image_url",
+                            "image_url": {
+                                "url": f"data:image/jpeg;base64,{image_base64}",
+                                "detail": "low",
+                            },
+                        },
+                    ],
+                }
+            ],
+            max_tokens=300,
+        )
+        return response.choices[0].message.content
+    except Exception:
+        return None
